84afd443cced292bba6918dab454a7b8c96e2152
Hannah Neary <hannah@evervault.com>
2024-09-17 14:17:47 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,30 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+addEventListener('fetch', event => {
+	event.respondWith(fetchScript(event))
+})
+
+async function fetchScript(event: FetchEvent): Promise<Response> {
+	
+	const targetScript = new URL(event.request.url.split(`${event.request.headers.get("host")}`)[1].replace(/^\/+/, ''));
+	const cache = caches.default
+	let response = await cache.match(event.request)
+	
+	if (!response) {
+		response = await fetch(targetScript)
+
+
+		if (response.status === 200) {
+
+		response = new Response(response.body, response)
+		response.headers.append('Cache-Control', 'public, max-age=86400') 
+			event.waitUntil(cache.put(event.request, response.clone()))
+		} else {
+			return new Response('Script not found', { status: 404 })
+		}
+	}
+
+	return response
+}
+

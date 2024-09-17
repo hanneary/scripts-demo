e2af5bc8534c252e85f41de60e2639be604356dc
Hannah Neary <hannah@evervault.com>
2024-09-17 16:50:51 +0100

    store hash

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,42 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 addEventListener('fetch', event => {
 	event.respondWith(fetchScript(event))
 })
+
+async function storeRefererHash(referer: string, hash: string) {
+	const expirationTtl = 86400;
+  
+	await REFERER_HASH_MAPPING.put(referer, hash, { expirationTtl });
+}
+
+async function getHashByReferer(referer: string) {
+	const hash = await REFERER_HASH_MAPPING.get(referer);
+	return hash;
+}
+
 
 async function fetchScript(event: FetchEvent): Promise<Response> {
 	
 	const targetScript = new URL(event.request.url.split(`${event.request.headers.get("host")}`)[1].replace(/^\/+/, ''));
 	const cache = caches.default
 	let response = await cache.match(event.request)
-	
+	let referrer = event.request.headers.get("referrer") || "ha"
+
+	const hash = await getHashByReferer(referrer)
+	console.log("Referer hash", hash)
 	if (!response) {
 		response = await fetch(targetScript)
-
+		const responseClone = response.clone()
 
 		if (response.status === 200) {
-
-		response = new Response(response.body, response)
-		response.headers.append('Cache-Control', 'public, max-age=86400') 
+			const responseBuffer = await responseClone.arrayBuffer()
+			const hash = await generateHash(responseBuffer)
+	
+			console.log("Script hash", hash)
+			response = new Response(response.body, response)
+			response.headers.append('Cache-Control', 'public, max-age=86400') 
 			event.waitUntil(cache.put(event.request, response.clone()))
+			event.waitUntil(storeRefererHash(referrer, hash))
 		} else {
 			return new Response('Script not found', { status: 404 })
 		}
@@ -38,3 +45,11 @@ async function fetchScript(event: FetchEvent): Promise<Response> {
 	return response
 }
 
+async function generateHash(data: ArrayBuffer) {
+  
+	const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+	const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  
+	return hashHex;
+}

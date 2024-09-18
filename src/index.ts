ee435d23f1c5ca21faf4c0fd1e63429366a325d0
Hannah Neary <hannah@evervault.com>
2024-09-18 17:28:53 +0100

    add script rewriter

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ addEventListener('fetch', event => {
 	event.respondWith(fetchScript(event))
 })
 
+const EXPIRATION_TTL = 500;
+
 async function storeRefererHash(referer: string, hash: string) {
-	const expirationTtl = 86400;
+	const expirationTtl = 86400000;
   
-	await REFERER_HASH_MAPPING.put(referer, hash, { expirationTtl });
+	await REFERER_HASH_MAPPING.put(referer, hash, { EXPIRATION_TTL });
 }
 
 async function getHashByReferer(referer: string) {
@@ -20,11 +22,12 @@ async function fetchScript(event: FetchEvent): Promise<Response> {
 	const targetScript = new URL(event.request.url.split(`${event.request.headers.get("host")}`)[1].replace(/^\/+/, ''));
 	const cache = caches.default
 	let response = await cache.match(event.request)
-	let referrer = event.request.headers.get("referrer") || "ha"
+	let referrer = event.request.headers.get("referrer") || "none"
 
 	const hash = await getHashByReferer(referrer)
 	console.log("Referer hash", hash)
-	if (!response) {
+	
+	if (true) {
 		response = await fetch(targetScript)
 		const responseClone = response.clone()
 
@@ -34,7 +37,6 @@ async function fetchScript(event: FetchEvent): Promise<Response> {
 	
 			console.log("Script hash", hash)
 			response = new Response(response.body, response)
-			response.headers.append('Cache-Control', 'public, max-age=86400') 
 			event.waitUntil(cache.put(event.request, response.clone()))
 			event.waitUntil(storeRefererHash(referrer, hash))
 		} else {
@@ -46,7 +48,6 @@ async function fetchScript(event: FetchEvent): Promise<Response> {
 }
 
 async function generateHash(data: ArrayBuffer) {
-  
 	const hashBuffer = await crypto.subtle.digest('SHA-256', data);
 	const hashArray = Array.from(new Uint8Array(hashBuffer));
 	const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
